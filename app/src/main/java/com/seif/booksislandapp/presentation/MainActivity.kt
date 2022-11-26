2d6b47ab4741fc6935248a9f7269afed854a83a8
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-11-26 23:58:35 +0200

    Project Packages Added

--- app/src/main/java/com/seif/booksislandapp/presentation/MainActivity.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.seif.booksislandapp
+package com.seif.booksislandapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.seif.booksislandapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
