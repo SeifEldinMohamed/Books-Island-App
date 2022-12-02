c3a2db6d8158939196b6c0eeea6d4f53a0674a2f
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-12-02 23:09:45 +0200

    Intro activity added

---
+++ app/src/main/java/com/seif/booksislandapp/presentation/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.seif.booksislandapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.seif.booksislandapp.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
