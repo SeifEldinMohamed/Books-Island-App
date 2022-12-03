4b92908542e8a549c64eef4de30eb7a9baf712ad
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-12-03 13:04:01 +0200

    - IntroActivity.kt and HomeActivity.kt Added

--- app/src/main/java/com/seif/booksislandapp/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/home/HomeActivity.kt
@@ -1,12 +1,12 @@
-package com.seif.booksislandapp.presentation
+package com.seif.booksislandapp.presentation.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.seif.booksislandapp.R
 
-class IntroActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        setContentView(R.layout.activity_home)
     }
 }
