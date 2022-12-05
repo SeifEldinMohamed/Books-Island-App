a5948785995dc8f91275aab534d6cd16b5b8cb31
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-12-05 12:26:21 +0200

    - initialize navController with introNavHostFragment - mMake IntroActivity.kt launcher

--- app/src/main/java/com/seif/booksislandapp/presentation/intro/IntroActivity.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/intro/IntroActivity.kt
@@ -2,11 +2,20 @@ package com.seif.booksislandapp.presentation.intro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.seif.booksislandapp.R
+import com.seif.booksislandapp.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.introNavHostFragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
     }
 }
--- app/src/main/java/com/seif/booksislandapp/presentation/intro/authentication/forget_password/ForgetPasswordFragment.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/intro/authentication/forget_password/ForgetPasswordFragment.kt
@@ -7,14 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.seif.booksislandapp.R
 
-
 class ForgetPasswordFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_forget_password, container, false)
     }
-
 }
