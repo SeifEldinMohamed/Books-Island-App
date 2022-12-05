3e89a0402456440b9ef297d183e476f490424197
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-12-05 12:50:34 +0200

    - main_nav_graph.xml Added - chats, home, myAds, profile and wishlist fragments created

---
+++ app/src/main/java/com/seif/booksislandapp/presentation/home/my_ads/MyAdsFragment.kt
@@ -0,0 +1,19 @@
+package com.seif.booksislandapp.presentation.home.my_ads
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.seif.booksislandapp.R
+
+class MyAdsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_ads, container, false)
+    }
+}
