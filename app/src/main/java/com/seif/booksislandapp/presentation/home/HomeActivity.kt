87cb6a46d31bbf9a0d8cd6e7bd2b21487c0a78a6
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-12-07 19:56:56 +0200

    - Add Home Fragment - Bottom Navigation Bar Added - link bottom navigation bar with nav controller

--- app/src/main/java/com/seif/booksislandapp/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/home/HomeActivity.kt
@@ -2,11 +2,55 @@ package com.seif.booksislandapp.presentation.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.seif.booksislandapp.R
+import com.seif.booksislandapp.databinding.ActivityHomeBinding
+import com.seif.booksislandapp.utils.hide
+import com.seif.booksislandapp.utils.show
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+    private lateinit var navController: NavController
+    private val TAG = "HomeActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigationComponent()
+
+        binding.fabProfile.setOnClickListener {
+            navController.navigate(R.id.profileFragment)
+        }
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.profileFragment -> {
+                    binding.bottomAppBar.hide()
+                    binding.fabProfile.hide()
+                }
+                else -> {
+                    binding.bottomAppBar.show()
+                    binding.fabProfile.show()
+                }
+            }
+        }
+    }
+
+    private fun setupNavigationComponent() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainNavHostFragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() ||
+            super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/seif/booksislandapp/presentation/home/home/HomeFragment.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/home/home/HomeFragment.kt
@@ -10,7 +10,8 @@ import com.seif.booksislandapp.R
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
