3d91e6e5ad6a7a526df7e10ce8efd6d2946d4486
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-12-07 20:01:12 +0200

    - add some icons in drawable - ChatsFragment.kt Added

--- app/src/main/java/com/seif/booksislandapp/presentation/home/chats/ChatsFragment.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/home/chats/ChatsFragment.kt
@@ -9,7 +9,8 @@ import com.seif.booksislandapp.R
 
 class ChatsFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
