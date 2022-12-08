bb7afb828dcf89a1d2170d524a381805f54fe7ad
hazem khaled <hazemk778@gmail.com>
2022-12-09 00:44:11 +0200

    onBoarding screens completely done and view pager created successfully

--- app/src/main/java/com/seif/booksislandapp/presentation/intro/onboarding/onboarding1/OnBoarding1Fragment.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/intro/onboarding/onboarding1/OnBoarding1Fragment.kt
@@ -5,16 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.seif.booksislandapp.R
+import com.seif.booksislandapp.databinding.FragmentOnboarding1Binding
 
 class OnBoarding1Fragment : Fragment() {
-
+    private lateinit var binding: FragmentOnboarding1Binding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = FragmentOnboarding1Binding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onboarding1, container, false)
+        // return inflater.inflate(R.layout.fragment_onboarding1, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnNext.setOnClickListener {
+            val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager)
+            viewPager?.currentItem = 1
+        }
     }
 }
--- app/src/main/java/com/seif/booksislandapp/presentation/intro/onboarding/viewpager/ViewPagerFragment.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/intro/onboarding/viewpager/ViewPagerFragment.kt
@@ -5,15 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.seif.booksislandapp.R
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.seif.booksislandapp.databinding.FragmentViewPagerBinding
 
 class ViewPagerFragment : Fragment() {
+    private lateinit var binding: FragmentViewPagerBinding
+    private lateinit var viewPager: ViewPager2
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = FragmentViewPagerBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_pager, container, false)
+        // return inflater.inflate(R.layout.fragment_view_pager, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val dotsIndicator = binding.dotsIndicator
+        viewPager = binding.viewPager
+        viewPager.adapter = ViewPagerAdapter(requireContext() as FragmentActivity)
+        dotsIndicator.attachTo(viewPager)
     }
 }
