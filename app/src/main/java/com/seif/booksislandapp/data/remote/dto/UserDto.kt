fddb2bde360f555796f05dbcc79c817fd9365df0
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-12-15 19:26:20 +0200

    - Mapper.kt and UserDto.kt Added

---
+++ app/src/main/java/com/seif/booksislandapp/data/remote/dto/UserDto.kt
@@ -0,0 +1,11 @@
+package com.seif.booksislandapp.data.remote.dto
+
+data class UserDto(
+    val id: String = "",
+    val avatarImage: String = "",
+    val username: String = "",
+    val email: String = "",
+    val password: String = "",
+    val address: String = "",
+    val gender: String = ""
+)
