ec107325bd82883041ff238430704f089787fade
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-01-06 22:19:22 +0200

    - mapper bet Advertisement and AdvertisementDto Added - AdvertisementRepositoryImp.kt Added - remove delay from auth repository

--- app/src/main/java/com/seif/booksislandapp/data/mapper/Mapper.kt
+++ app/src/main/java/com/seif/booksislandapp/data/mapper/Mapper.kt
@@ -1,6 +1,8 @@
 package com.seif.booksislandapp.data.mapper
 
+import com.seif.booksislandapp.data.remote.dto.AdvertisementDto
 import com.seif.booksislandapp.data.remote.dto.UserDto
+import com.seif.booksislandapp.domain.model.Advertisement
 import com.seif.booksislandapp.domain.model.User
 
 fun User.toUserDto(): UserDto {
@@ -26,5 +28,26 @@ fun UserDto.toUser(): User {
         governorate = this.governorate,
         district = this.district,
         gender = this.gender
+    )
+}
+
+fun AdvertisementDto.toAdvertisement(): Advertisement {
+    return Advertisement(
+        id = id,
+        owner = owner,
+        book = book,
+        isDonateAdv = isDonateAdv,
+        status = status,
+        publishTime = publishTime
+    )
+}
+fun Advertisement.toAdvertisementDto(): AdvertisementDto {
+    return AdvertisementDto(
+        id = id,
+        owner = owner,
+        book = book,
+        isDonateAdv = isDonateAdv,
+        status = status,
+        publishTime = publishTime
     )
 }
