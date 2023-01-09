e70e58b0f12b5302fe133bf34d465614d7b4e229
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-01-09 23:54:02 +0200

    - update design of UploadSellAdvertismentFragment

--- app/src/main/java/com/seif/booksislandapp/presentation/home/upload_advertisement/UploadSellAdvertisementFragment.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/home/upload_advertisement/UploadSellAdvertisementFragment.kt
@@ -7,19 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import com.seif.booksislandapp.R
-import com.seif.booksislandapp.databinding.FragmentUploadAdvertisementBinding
+import com.seif.booksislandapp.databinding.FragmentUploadSellAdvertisementBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class UploadAdvertisementFragment : Fragment() {
-    lateinit var binding: FragmentUploadAdvertisementBinding
+class UploadSellAdvertisementFragment : Fragment() {
+    lateinit var binding: FragmentUploadSellAdvertisementBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        binding = FragmentUploadAdvertisementBinding.inflate(inflater, container, false)
+        binding = FragmentUploadSellAdvertisementBinding.inflate(inflater, container, false)
         return binding.root
     }
 
