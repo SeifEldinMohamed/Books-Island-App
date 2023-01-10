a956bed957354baf34faacd47005729e073f9ed4
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-01-10 20:46:15 +0200

    - add some initialization in UploadSellAdvertisementFragment - add ic_book_category.xml icon

---
+++ app/src/main/java/com/seif/booksislandapp/presentation/home/categories/book_categories/BookCategoriesFragment.kt
@@ -0,0 +1,24 @@
+package com.seif.booksislandapp.presentation.home.categories.book_categories
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.seif.booksislandapp.R
+
+class BookCategoriesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
