bae9750df8c1ca1196072858d23821ba3d8e144e
heba <hebaadel1004@gmail.com>
2023-01-28 15:16:43 +0200

    login page , forget password

--- app/src/main/java/com/seif/booksislandapp/presentation/home/upload_advertisement/sell/UploadSellAdvertisementViewModel.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/home/upload_advertisement/sell/UploadSellAdvertisementViewModel.kt
@@ -6,5 +6,6 @@ import javax.inject.Inject
 
 class UploadSellAdvertisementViewModel @Inject constructor(
     private val uploadSellAdvertisementUseCase: UploadSellAdvertisementUseCase
-): ViewModel() {
+) : ViewModel(){
+
 }
--- app/src/main/java/com/seif/booksislandapp/presentation/intro/authentication/forget_password/ForgetPasswordFragment.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/intro/authentication/forget_password/ForgetPasswordFragment.kt
@@ -5,15 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.seif.booksislandapp.R
+import androidx.navigation.fragment.findNavController
+import com.seif.booksislandapp.databinding.FragmentForgetPasswordBinding
 
 class ForgetPasswordFragment : Fragment() {
+    private lateinit var binding: FragmentForgetPasswordBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forget_password, container, false)
+    ): View {
+        binding = FragmentForgetPasswordBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.ivBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 }
