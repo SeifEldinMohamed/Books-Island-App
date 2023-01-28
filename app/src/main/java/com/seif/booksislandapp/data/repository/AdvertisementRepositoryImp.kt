a1369680561084cc44c005e05888a10e87fb9352
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-01-28 09:40:23 +0200

    - edit some functions in AdvertisementRepositoryImp.kt - add root directory constant

--- app/src/main/java/com/seif/booksislandapp/data/repository/AdvertisementRepositoryImp.kt
+++ app/src/main/java/com/seif/booksislandapp/data/repository/AdvertisementRepositoryImp.kt
@@ -5,10 +5,10 @@ import android.net.Uri
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.StorageReference
 import com.seif.booksislandapp.R
-import com.seif.booksislandapp.data.mapper.toAdvertisement
-import com.seif.booksislandapp.data.mapper.toAdvertisementDto
-import com.seif.booksislandapp.data.remote.dto.AdvertisementDto
-import com.seif.booksislandapp.domain.model.Advertisement
+import com.seif.booksislandapp.data.mapper.toSellAdvertisement
+import com.seif.booksislandapp.data.mapper.toSellAdvertisementDto
+import com.seif.booksislandapp.data.remote.dto.adv.SellAdvertisementDto
+import com.seif.booksislandapp.domain.model.adv.SellAdvertisement
 import com.seif.booksislandapp.domain.repository.AdvertisementRepository
 import com.seif.booksislandapp.utils.Constants.Companion.SELL_ADVERTISEMENT_COLLECTION
 import com.seif.booksislandapp.utils.Resource
@@ -16,6 +16,7 @@ import com.seif.booksislandapp.utils.ResourceProvider
 import com.seif.booksislandapp.utils.checkInternetConnection
 import kotlinx.coroutines.*
 import kotlinx.coroutines.tasks.await
+import timber.log.Timber
 import java.lang.Exception
 import javax.inject.Inject
 
@@ -25,21 +26,21 @@ class AdvertisementRepositoryImp @Inject constructor(
     private val resourceProvider: ResourceProvider,
     private val connectivityManager: ConnectivityManager
 ) : AdvertisementRepository {
-    override suspend fun getAllSellAds(): Resource<List<Advertisement>, String> {
+    override suspend fun getAllSellAds(): Resource<List<SellAdvertisement>, String> {
         if (!connectivityManager.checkInternetConnection())
             return Resource.Error(resourceProvider.string(R.string.no_internet_connection))
         return try {
             delay(1000) // to show loading progress
 
             val querySnapshot = firestore.collection(SELL_ADVERTISEMENT_COLLECTION).get().await()
-            val sellAdvertisements = arrayListOf<AdvertisementDto>()
+            val sellAdvertisementsDto = arrayListOf<SellAdvertisementDto>()
             for (document in querySnapshot) {
-                val advertisement = document.toObject(AdvertisementDto::class.java)
-                sellAdvertisements.add(advertisement)
+                val sellAdvertisementDto = document.toObject(SellAdvertisementDto::class.java)
+                sellAdvertisementsDto.add(sellAdvertisementDto)
             }
             Resource.Success(
-                data = sellAdvertisements.map { advertisementDto ->
-                    advertisementDto.toAdvertisement()
+                data = sellAdvertisementsDto.map { sellAdvertisementDto ->
+                    sellAdvertisementDto.toSellAdvertisement()
                 }.toCollection(ArrayList())
             )
         } catch (e: Exception) {
@@ -47,16 +48,22 @@ class AdvertisementRepositoryImp @Inject constructor(
         }
     }
 
-    override suspend fun addSellAd(advertisement: Advertisement): Resource<String, String> {
-        return when (val result = uploadMultipleImages(advertisement.book.images)) {
+    override suspend fun uploadSellAdv(sellAdvertisement: SellAdvertisement): Resource<String, String> {
+        if (!connectivityManager.checkInternetConnection())
+            return Resource.Error(resourceProvider.string(R.string.no_internet_connection))
+
+        return when (val result = uploadMultipleImages(sellAdvertisement.book.images)) {
             is Resource.Error -> {
+                Timber.d("uploadSellAdv: Error  ${result.message}")
                 Resource.Error(result.message)
             }
             is Resource.Success -> {
                 try {
                     val document = firestore.collection(SELL_ADVERTISEMENT_COLLECTION).document()
-                    advertisement.id = document.id
-                    document.set(advertisement.toAdvertisementDto()).await()
+                    sellAdvertisement.id = document.id
+                    sellAdvertisement.book.images = result.data
+                    document.set(sellAdvertisement.toSellAdvertisementDto()).await()
+                    Timber.d("uploaded successfully")
                     Resource.Success("Advertisement Added Successfully with id : ${document.id}")
                 } catch (e: Exception) {
                     Resource.Error(e.message.toString())
@@ -83,8 +90,10 @@ class AdvertisementRepositoryImp @Inject constructor(
                     }
                 }.awaitAll()
             }
+            Timber.d("upload images Success: $uris")
             Resource.Success(uris)
         } catch (e: Exception) {
+            Timber.d("upload images Error: ${e.message}")
             Resource.Error(e.message.toString())
         }
     }
