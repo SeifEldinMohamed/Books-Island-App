6439c27fa3c379633515b41d2be32e6d2ea62737
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-01-31 08:36:38 +0200

    - provide firebase storage instance - provide advertisementRepository instance - edit repositories interfaces

--- app/src/main/java/com/seif/booksislandapp/domain/repository/AdvertisementRepository.kt
+++ app/src/main/java/com/seif/booksislandapp/domain/repository/AdvertisementRepository.kt
@@ -1,9 +1,9 @@
 package com.seif.booksislandapp.domain.repository
 
-import com.seif.booksislandapp.domain.model.Advertisement
+import com.seif.booksislandapp.domain.model.adv.SellAdvertisement
 import com.seif.booksislandapp.utils.Resource
 
 interface AdvertisementRepository {
-    suspend fun getAllSellAds(): Resource<List<Advertisement>, String>
-    suspend fun addSellAd(advertisement: Advertisement): Resource<String, String>
+    suspend fun getAllSellAds(): Resource<List<SellAdvertisement>, String>
+    suspend fun uploadSellAdv(sellAdvertisement: SellAdvertisement): Resource<String, String>
 }
