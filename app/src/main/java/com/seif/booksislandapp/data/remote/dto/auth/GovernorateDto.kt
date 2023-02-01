bb04009100b8fcbc8e6993435f2c863b744ba8c5
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-02-01 08:53:28 +0200

    - DistrictDto.kt and GovernorateDto.kt Added - add some constants

---
+++ app/src/main/java/com/seif/booksislandapp/data/remote/dto/auth/GovernorateDto.kt
@@ -0,0 +1,6 @@
+package com.seif.booksislandapp.data.remote.dto.auth
+
+data class GovernorateDto(
+    val id: String = "",
+    val name: String = ""
+)
--- app/src/main/java/com/seif/booksislandapp/utils/Constants.kt
+++ app/src/main/java/com/seif/booksislandapp/utils/Constants.kt
@@ -2,23 +2,31 @@ package com.seif.booksislandapp.utils
 
 class Constants {
     companion object {
+        // Pagination
         const val PAGE_SIZE = 15
 
-        // firebase
-        const val USER_FireStore_Collection = "users"
-        const val Governments_FireStore_Collection = "governments"
-        const val qesm_FireStore_Collection = "Districts"
+        // FireStore
+        const val USER_FIRESTORE_COLLECTION = "users"
+        const val GOVERNORATES_FIRESTORE_COLLECTION = "governorate"
+        const val DISTRICTS_FIRESTORE_COLLECTION = "districts"
+        const val SELL_ADVERTISEMENT_FIRESTORE_COLLECTION = "sell advertisements"
+
+        // Storage
         const val FIREBASE_STORAGE_ROOT_DIRECTORY = "app"
 
-        // shared preference keys
-        const val USER_KEY = "user"
+        // Shared Preference Keys
+        const val USER_ID_KEY = "user_id"
+        const val USERNAME_KEY = "user_name"
+        const val USER_GOVERNORATE_KEY = "user_governorate"
+        const val USER_DISTRICT_KEY = "user_district"
+        const val USER_AVATAR_KEY = "user_avatar"
         const val IS_FIRST_TIME_KEY = "is_first_time"
         const val IS_LOGGED_IN_KEY = "is_logged_in"
 
-        //
-        const val SELL_ADVERTISEMENT_COLLECTION = "sell advertisements"
+        // Upload
         const val MAX_UPLOADED_IMAGES_NUMBER = 5
-        // splash
+
+        // Splash
         const val HANDLER_DELAY = 2000L
         const val MAX_PROGRESS_BAR = 1000
         const val CURRENT_PROGRESS_ANIMATION = 1500
