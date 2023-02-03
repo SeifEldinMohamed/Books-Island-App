2309d5e0f8082501a31b8d37b45bdfb8e22498ef
heba <hebaadel1004@gmail.com>
2023-02-03 06:15:02 +0200

    home page ,(text size:ssp,id),profile,my ads

--- app/src/main/java/com/seif/booksislandapp/presentation/home/profile/ProfileFragment.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/home/profile/ProfileFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.seif.booksislandapp.R
+import androidx.navigation.fragment.findNavController
+import com.seif.booksislandapp.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
-
+    private lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.ivBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 }
