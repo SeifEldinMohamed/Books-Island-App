5445d2f78a9bf59d75873a96ecdb9e513d0a5281
heba <hebaadel1004@gmail.com>
2023-02-04 21:35:41 +0200

    back of buy fragment

--- app/src/main/java/com/seif/booksislandapp/presentation/home/categories/buy/BuyFragment.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/home/categories/buy/BuyFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.seif.booksislandapp.R
+import androidx.navigation.fragment.findNavController
+import com.seif.booksislandapp.databinding.FragmentBuyBinding
 
 class BuyFragment : Fragment() {
-
+    private lateinit var binding: FragmentBuyBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_buy, container, false)
+        binding = FragmentBuyBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.ivBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 }
