624469388b8407229d603832f65f9643c6fcc3ad
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-02-06 10:29:12 +0200

    - add searchSellAdv logic in AdverisementRepositoryImp.kt - SearchSellAdvertisementUseCase.kt Added

---
+++ app/src/main/java/com/seif/booksislandapp/domain/usecase/usecase/advertisement/sell/SearchSellAdvertisementUseCase.kt
@@ -0,0 +1,14 @@
+package com.seif.booksislandapp.domain.usecase.usecase.advertisement.sell
+
+import com.seif.booksislandapp.data.repository.AdvertisementRepositoryImp
+import com.seif.booksislandapp.domain.model.adv.SellAdvertisement
+import com.seif.booksislandapp.utils.Resource
+import javax.inject.Inject
+
+class SearchSellAdvertisementUseCase @Inject constructor(
+    private val advertisementRepositoryImp: AdvertisementRepositoryImp
+) {
+    suspend operator fun invoke(searchQuery: String): Resource<ArrayList<SellAdvertisement>, String> {
+        return advertisementRepositoryImp.searchSellAdv(searchQuery)
+    }
+}
