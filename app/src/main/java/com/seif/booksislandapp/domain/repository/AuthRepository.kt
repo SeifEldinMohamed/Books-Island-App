0aaaa6fa514567941d0901c3d2541290fab42d35
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-03-02 14:46:31 +0200

    - AuthRepository.kt and UserRepository.kt Added - edit GetFirebaseCurrentUserUseCase.kt

--- app/src/main/java/com/seif/booksislandapp/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/seif/booksislandapp/domain/repository/AuthRepository.kt
@@ -1,6 +1,5 @@
 package com.seif.booksislandapp.domain.repository
 
-import com.google.firebase.auth.FirebaseUser
 import com.seif.booksislandapp.domain.model.User
 import com.seif.booksislandapp.domain.model.auth.District
 import com.seif.booksislandapp.domain.model.auth.Governorate
@@ -11,7 +10,6 @@ interface AuthRepository {
     suspend fun login(email: String, password: String): Resource<String, String>
     suspend fun forgetPassword(email: String): Resource<String, String>
     suspend fun logout(): Resource<String, String>
-    fun getFirebaseCurrentUser(): FirebaseUser?
     fun <T> saveInSharedPreference(key: String, data: T)
     fun <T> getFromSharedPreference(key: String, clazz: Class<T>): T
     suspend fun getGovernorates(): Resource<List<Governorate>, String>
