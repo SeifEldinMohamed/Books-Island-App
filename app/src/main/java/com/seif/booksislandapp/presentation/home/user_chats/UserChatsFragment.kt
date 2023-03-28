69ad307ca8ee6adde39c83b216f3abb67511235b
heba <hebaadel1004@gmail.com>
2023-03-28 02:56:33 +0200

    users chat design

--- app/src/main/java/com/seif/booksislandapp/presentation/home/user_chats/UserChatsFragment.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/home/user_chats/UserChatsFragment.kt
@@ -1,19 +1,40 @@
 package com.seif.booksislandapp.presentation.home.user_chats
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.google.android.material.tabs.TabLayoutMediator
 import com.seif.booksislandapp.R
+import com.seif.booksislandapp.databinding.FragmentUserChatsBinding
+import com.seif.booksislandapp.presentation.home.user_chats.adapter.UserChatPagerAdapter
 
 class UserChatsFragment : Fragment() {
+    private lateinit var binding: FragmentUserChatsBinding
+    private val tabTitle = arrayListOf(" Buying ", " Selling ")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_chats, container, false)
+    ): View {
+        binding = FragmentUserChatsBinding.inflate(layoutInflater)
+        setupTabLayoutWithViewPager()
+        return binding.root
+    }
+    @SuppressLint("InflateParams")
+    private fun setupTabLayoutWithViewPager() {
+        binding.chatViewPager.adapter = UserChatPagerAdapter(this)
+        TabLayoutMediator(binding.tlChat, binding.chatViewPager) { tab, position ->
+            tab.text = tabTitle[position]
+        }.attach()
+        for (i in 0..1) {
+            val textView =
+                LayoutInflater.from(requireContext()).inflate(R.layout.my_ads_tab_title, null)
+                    as TextView
+            binding.tlChat.getTabAt(i)?.customView = textView
+        }
     }
 }
---
+++ app/src/main/java/com/seif/booksislandapp/presentation/home/user_chats/fragments/BuyingChat.kt
@@ -0,0 +1,6 @@
+package com.seif.booksislandapp.presentation.home.user_chats.fragments
+
+import androidx.fragment.app.Fragment
+import com.seif.booksislandapp.R
+
+class BuyingChat : Fragment(R.layout.buying_chat)
