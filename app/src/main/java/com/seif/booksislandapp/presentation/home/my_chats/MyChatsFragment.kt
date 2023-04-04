dc01027e988e5012946cbd87d3dca873a36ec757
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-04-04 17:45:04 +0200

    - fragment_my_chats.xml - selling_chat.xml - my_chat_item.xml - ChatRepositoryImp.kt - mapper.kt

--- app/src/main/java/com/seif/booksislandapp/presentation/home/my_chats/MyChatsFragment.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/home/my_chats/MyChatsFragment.kt
@@ -1,19 +1,52 @@
 package com.seif.booksislandapp.presentation.home.my_chats
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
 import com.seif.booksislandapp.R
+import com.seif.booksislandapp.databinding.FragmentMyChatsBinding
+import com.seif.booksislandapp.presentation.home.my_chats.adapter.UserChatPagerAdapter
 
 class MyChatsFragment : Fragment() {
+    private var _binding: FragmentMyChatsBinding? = null
+    private val binding get() = _binding!!
+    private val tabTitle = arrayListOf(" Buying ", " Selling ")
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_chats, container, false)
+    ): View {
+        _binding = FragmentMyChatsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupTabLayoutWithViewPager()
+    }
+
+    @SuppressLint("InflateParams")
+    private fun setupTabLayoutWithViewPager() {
+        binding.chatViewPager.adapter = UserChatPagerAdapter(this)
+        TabLayoutMediator(binding.tlChat, binding.chatViewPager) { tab, position ->
+            tab.text = tabTitle[position]
+        }.attach()
+        for (i in 0..1) {
+            val textView =
+                LayoutInflater.from(requireContext())
+                    .inflate(R.layout.my_ads_tab_title, null) as TextView
+            binding.tlChat.getTabAt(i)?.customView = textView
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
--- app/src/main/java/com/seif/booksislandapp/presentation/home/my_chats/adapter/UserChatPagerAdapter.kt
+++ app/src/main/java/com/seif/booksislandapp/presentation/home/my_chats/adapter/UserChatPagerAdapter.kt
@@ -1,17 +1,17 @@
-package com.seif.booksislandapp.presentation.home.user_chats.adapter
+package com.seif.booksislandapp.presentation.home.my_chats.adapter
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.seif.booksislandapp.presentation.home.user_chats.fragments.BuyingChat
-import com.seif.booksislandapp.presentation.home.user_chats.fragments.SellingChat
+import com.seif.booksislandapp.presentation.home.my_chats.fragments.SellingChat
+import com.seif.booksislandapp.presentation.home.my_chats.fragments.buying_chats.BuyingChatFragment
 
 class UserChatPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
     override fun getItemCount(): Int = 2
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> SellingChat()
-            else -> BuyingChat()
+            0 -> BuyingChatFragment()
+            else -> SellingChat()
         }
     }
 }
