6168fcee0bf1d12b0f4ed7242006b2545f7d4e28
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-06-09 07:43:02 +0300

    - reportUser logic added in UserRepositoryImpl.kt

--- app/src/main/java/com/seif/booksislandapp/domain/repository/UserRepository.kt
+++ app/src/main/java/com/seif/booksislandapp/domain/repository/UserRepository.kt
@@ -1,6 +1,7 @@
 package com.seif.booksislandapp.domain.repository
 
 import com.google.firebase.auth.FirebaseUser
+import com.seif.booksislandapp.domain.model.Report
 import com.seif.booksislandapp.domain.model.User
 import com.seif.booksislandapp.utils.Resource
 
@@ -8,4 +9,5 @@ interface UserRepository {
     fun getFirebaseCurrentUser(): FirebaseUser?
     suspend fun updateUserProfile(user: User): Resource<User, String>
     suspend fun fetchUsersIChatWith(currentUserId: String): Resource<List<User>, String>
+    suspend fun reportUser(report: Report): Resource<String, String>
 }
