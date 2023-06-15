2b62c8853a150b69d20d864f788dcc115eb86edf
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-06-15 08:23:28 +0300

    - AdProviderProfileViewModel.kt Added

---
+++ app/src/main/java/com/seif/booksislandapp/presentation/home/ad_provider_profile/AdProviderProfileViewModel.kt
@@ -0,0 +1,74 @@
+package com.seif.booksislandapp.presentation.home.ad_provider_profile
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.seif.booksislandapp.R
+import com.seif.booksislandapp.domain.usecase.usecase.user.GetUserByIdUseCase
+import com.seif.booksislandapp.utils.Resource
+import com.seif.booksislandapp.utils.ResourceProvider
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@HiltViewModel
+class AdProviderProfileViewModel @Inject constructor(
+    private val resourceProvider: ResourceProvider,
+    private val getUserByIdUseCase: GetUserByIdUseCase,
+
+    ) : ViewModel() {
+    private var _adProviderProfileState =
+        MutableStateFlow<AdProviderProfileState>(AdProviderProfileState.Init)
+    val adProviderProfileState = _adProviderProfileState.asStateFlow()
+
+    fun getAdProviderUserById(currUserId: String) {
+        setLoading(true)
+        viewModelScope.launch(Dispatchers.IO) {
+            getUserByIdUseCase(currUserId).let {
+                when (it) {
+                    is Resource.Error -> {
+                        withContext(Dispatchers.Main) {
+                            setLoading(false)
+                            showError(it.message)
+                        }
+                    }
+
+                    is Resource.Success -> {
+                        withContext(Dispatchers.Main) {
+                            setLoading(false)
+                        }
+                        _adProviderProfileState.value =
+                            AdProviderProfileState.FetchAdProviderUserSuccessfully(it.data)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setLoading(status: Boolean) {
+        when (status) {
+            true -> {
+                _adProviderProfileState.value = AdProviderProfileState.IsLoading(true)
+            }
+
+            false -> {
+                _adProviderProfileState.value = AdProviderProfileState.IsLoading(false)
+            }
+        }
+    }
+
+    private fun showError(message: String) {
+        when (message) {
+            resourceProvider.string(R.string.no_internet_connection) -> {
+                _adProviderProfileState.value = AdProviderProfileState.NoInternetConnection(message)
+            }
+
+            else -> {
+                _adProviderProfileState.value = AdProviderProfileState.ShowError(message)
+            }
+        }
+    }
+}
