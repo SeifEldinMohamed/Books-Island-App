aca22f0372dd554c65769ed7367807f38bceb3dd
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-06-24 20:37:09 +0300

    - add reportedPersonName, reporterName and reviewed attributes in Report

--- app/src/main/java/com/seif/booksislandapp/domain/model/Report.kt
+++ app/src/main/java/com/seif/booksislandapp/domain/model/Report.kt
@@ -3,7 +3,10 @@ package com.seif.booksislandapp.domain.model
 data class Report(
     var id: String,
     val reporterId: String,
+    val reporterName: String,
     val reportedPersonId: String,
+    val reportedPersonName: String,
     val comment: String,
     val category: String,
+    val isReviewed: Boolean = false
 )
