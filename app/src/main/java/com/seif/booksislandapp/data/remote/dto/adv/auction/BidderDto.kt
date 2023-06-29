cde3f1c4a0d3753921bc3432825f2b82f2e1a0a4
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-06-29 17:14:40 +0300

    - add strings - add navigation from upload auction to bidders history - add bidder avatar in bidder.kt

--- app/src/main/java/com/seif/booksislandapp/data/remote/dto/adv/auction/BidderDto.kt
+++ app/src/main/java/com/seif/booksislandapp/data/remote/dto/adv/auction/BidderDto.kt
@@ -3,5 +3,6 @@ package com.seif.booksislandapp.data.remote.dto.adv.auction
 data class BidderDto(
     val bidderId: String = "",
     val bidderName: String = "",
+    val bidderAvatar: String = "",
     val suggestedPrice: Int = 0
 )
